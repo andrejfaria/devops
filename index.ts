474d736d75d9a0344c18183b4801f141e1285f9a
andre faria <andre.faria@ceiia.com>
2023-07-13 13:52:01 +0100

    added ecr repo

--- index.ts
+++ index.ts
@@ -1,7 +1,12 @@
 import { DpsFrontend } from "./services/frontend";
+import { DpsBackend } from './services/backend'
 
 
 function main() {
+  new DpsBackend({
+    Name: "dps",
+    Product: "devops-backend",
+  })
   new DpsFrontend({
     Name: "dps",
     Product: "devops-frontend",
